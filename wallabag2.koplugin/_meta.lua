94f27da776b7f14ab87d798ca7e74d428e37c43f
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-08-28 22:04:12 +0700

    restructure

--- wallabag2.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "wallabag2",
-    fullname = _("Wallabag2"),
-    description = _([[Synchronises articles with a Wallabag version 2.x (2.4+) server.]]),
-}
