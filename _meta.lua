a91286436496b070b694ed7ef1c05a5068ed1891
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-08-28 21:51:43 +0700

    init async refactor

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "wallabag2",
+    fullname = _("Wallabag2"),
+    description = _([[Synchronises articles with a Wallabag version 2.x (2.4+) server.]]),
+}
